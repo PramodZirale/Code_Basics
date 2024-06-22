c6c73c45c1d70017b614b00a02f156c8cb1d5746
Pramod <p.zirale@taikishaindia.com>
2024-06-22 21:27:15 +0530

    [Variable  Quiz & Excersize]

---
+++ Variables.py
@@ -0,0 +1,43 @@
+""" 
+Q1.
+Which of the following variable names follows the snake case naming convention?
+
+1. Variable1 2. rateOfInterest 3. first_name 4. None
+
+Ans. : first_name
+
+Q2.
+Which is the correct variable name in Python?
+1. 2nd_variable 2. firstVariable 3. Third-variable 4. Last$Variable
+
+Ans. firstVariable
+
+Q3.
+Which of the following will store the value 100 to the variable in Python?
+1. 100 = variable 2. 100inThis = 100 3._hundred = 10*10 4. a = "100"
+
+Ans. _hundred = 10*10
+
+"""
+
+#Create a variable name pi and store the value 3.14 in it. check data type of this variable.
+pi = 3.14
+print(type(pi))
+
+#Create a variable called for and assign it a value 4. see what happens and find out the reason behind the behavior.
+for_ = 4
+
+print(for_)
+
+#Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years.
+principal_amount = 1000
+rate_of_interest = 10
+time = 3
+
+Simple_Interest = principal_amount * rate_of_interest * time / 100
+
+print(Simple_Interest)
+
+
+
+
