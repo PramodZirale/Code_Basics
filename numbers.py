dcde86fdd73de95ade6dc3bcb4035ac2656ed0be
Pramod <p.zirale@taikishaindia.com>
2024-06-22 22:16:21 +0530

    [numbers concept]

---
+++ numbers.py
@@ -0,0 +1,46 @@
+""" 
+Which of the following is not the correct number?
+a) 3j+4
+b) "3320"
+c) 2e-4
+d) 56.785
+
+Question 2
+What is the binary representation of the number 9?
+a) 1010
+b) 1101
+c) 0111
+d) 1001
+
+Question 3
+What is the datatype of a?
+
+python
+Copy code
+a = 1.2e3
+print(type(a))
+a) int
+b) str
+c) float
+d) set
+
+"""
+
+#Use formate function to print the following:145 & '0'
+
+print(format(145, 'o'))
+
+
+#There is a circular pond in a village. This pond has a radius of 84 meters. Can you find the area of the pond? 
+area = 3.14 * 84 * 84
+print(area)
+
+
+
+#(Bonus: If there is exactly 1.4 liter of water in a square meter, what is the total amount of water in the pond?)
+
+water_per_square_meter = 1.4  # Liters of water per square meter
+
+total_water = area * water_per_square_meter
+
+print(total_water)
