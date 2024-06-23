d7b81926d71cd8861c22c04a4f583b8ec9739424
Pramod <p.zirale@taikishaindia.com>
2024-06-23 22:59:02 +0530

    [functions Quiz & Excersize]

--- functions.py
+++ functions.py
@@ -16,3 +16,31 @@ print(sum_of_numbers([1, 2, 3, 4, 5])) # 15
 
 # print(help(sum_of_numbers)) : pycharm will show the docstring of the function
 
+#find valume of cylinder pi*r^2*h
+#positional orguments
+def cylinder_volume(radius, height):
+    """
+    Function to calculate the volume of a cylinder
+    :param radius: radius of the cylinder
+    :param height: height of the cylinder
+    :return: volume of the cylinder
+    """
+    pi = 3.14159
+    return pi * (radius ** 2) * height
+
+r= 5
+h = 10
+print(cylinder_volume(radius=r, height=h)) # 942.477
+
+#default arguments
+
+def cylinder_volume(radius, height=10):
+    """
+    Function to calculate the volume of a cylinder
+    :param radius: radius of the cylinder
+    :param height: height of the cylinder
+    :return: volume of the cylinder
+    """
+    pi = 3.14159
+    return pi * (radius ** 2) * height
+
