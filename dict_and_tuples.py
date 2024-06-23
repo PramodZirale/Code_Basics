b584f794bb47939895d7b6bfa2cd64d460aff26f
Pramod <p.zirale@taikishaindia.com>
2024-06-23 23:47:59 +0530

    [dict_tuple Quiz & Excersize]

--- dict_and_tuples.py
+++ dict_and_tuples.py
@@ -19,5 +19,38 @@ pe_ratio, pb_ratio = find_pe_and_pb(100, 10, 50)
 
 print(pe_ratio, pb_ratio)
 
+# dictionary is a key value pair data structure in python and is mutable like list 
+# dictionary is unordered collection of items
+# dictionary is defined using curly braces {}
+# dictionary is defined as key value pairs
+# dictionary is mutable
+# dictionary is unordered
+# dictionary is dynamic
+# dictionary is indexed
+# dictionary is nested
+# dictionary is iterable
 
+#study big o notation very important concept in data structures and algorithms in youtube
+
+# example without dict it will iterate through all the elements to find the value
+contacts = [('John Doe', 8867676767), ('Jane Doe', 7867676767), ('John Smith', 9867676767)]
+
+for contact in contacts:
+    if contact[0] == 'Jane Doe':
+        print(contact[1])
+        break
     
+# example with dict it will directly go to the key and get the value
+# memory management is the only difference between dict and list - array in youtube
+
+d = {
+    'ravi': 8867676767,
+    'john': 7867676767,
+    'smith': 9867676767,
+    'jane': 9867676767,
+}
+
+print(d['jane'])
+
+
+
