49d54f0f4d5412b464cda910fe2e6ed2bf564ee7
Pramod <p.zirale@taikishaindia.com>
2024-06-26 23:14:42 +0530

    Add Modules and Pip file

---
+++ Modules_and_Pip.py
@@ -0,0 +1,25 @@
+"""
+Q1. To use a module in a python file, you need to import it using _____ statement.
+
+a.  from
+b.  import
+c.  insert
+d.  bank
+
+Q2.
+Suppose you have created a multiply() function in a file calculate.py. You need to use that multiply function inside your other python program. How will you import and use it?
+
+
+a. import calculate
+   res = calculate.multiply(3, 5)
+
+b. import calculate 
+   res = multiply(3, 5)
+
+c. from calculate import multiply
+   res = calculate.multiply(3, 5)
+
+d. from multiply import calculate
+   res = multiply(3, 5)
+
+"""
