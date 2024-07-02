9d993f51443b85a1a6fd6da535974b9862961f59
Pramod <p.zirale@taikishaindia.com>
2024-07-02 23:27:03 +0530

    [file handling excersize]

---
+++ File_Handling/excersize.py
@@ -0,0 +1,9 @@
+# student_marks.csv contains the marks and other details for some students. Write a python program to:
+
+# Open the file in read mode
+# Create a dictionary from the given data
+# Add a new field to the dictionary total\_marks and store the total marks of the students.
+# Add a new field to the dictionary Average and store the average marks of the students.
+# Create a new file and write this information to the new file
+# (https://www.kaggle.com/arunkumar413/student-marks)
+
